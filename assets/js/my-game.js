5d6915de271f46ba3b4340d71daa9fbf98bd96c1
George Okwori <132067023+GOkwori@users.noreply.github.com>
2024-03-10 01:19:37 +0000

    Add variables to JScript

--- assets/js/my-game.js
+++ assets/js/my-game.js
@@ -4,3 +4,11 @@ const optionsEls = document.querySelectorAll(".option-text");
 const scoreEl = document.getElementById("score");
 const questionCounterEl = document.getElementById("questionCounter");
 const nextButton = document.getElementById("next-btn");
+
+// Variables for the game
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
