068859f5dbc5c920a29008eabf6eb81bd4831984
George Okwori <132067023+GOkwori@users.noreply.github.com>
2024-03-10 16:02:32 +0000

    Declare variables and add hard-coded questions

--- assets/js/my-game.js
+++ assets/js/my-game.js
@@ -1,316 +1,235 @@
-// DOM Elements
-const questionEl = document.getElementById("question");
-const optionsEls = document.querySelectorAll(".option-text");
-const scoreEl = document.getElementById("score");
-const questionCounterEl = document.getElementById("questionCounter");
-const nextButton = document.getElementById("next-btn");
+const queestion = document.getElementById('question');
+const options = Array.from(document.getElementsByClassName('option-text'));
 
-// Variables for the game
 let currentQuestion = {};
 let acceptingAnswers = true;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-// Questions
-let questions = {
-  easy: easyQuestions,
-  medium: mediumQuestions,
-  hard: hardQuestions,
-};
-
 // Easy Questions
 let easyQuestions = [
-  {
-    question: "What is the name of Mickey Mouse's pet dog?",
-    options: ["Goofy", "Pluto", "Max", "Fido"],
-    answer: "Pluto",
-  },
-  {
-    question: "Which cartoon character lives in a pineapple under the sea?",
-    options: [
-      "SpongeBob SquarePants",
-      "Patrick Star",
-      "Squidward Tentacles",
-      "Mr. Krabs",
-    ],
-    answer: "SpongeBob SquarePants",
-  },
-  {
-    question: "What color are the Smurfs?",
-    options: ["Green", "Blue", "Red", "Yellow"],
-    answer: "Blue",
-  },
-  {
-    question: "Who is the fastest character in Looney Tunes?",
-    options: ["Foghorn Leghorn", "Daffy Duck", "Road Runner", "Bugs Bunny"],
-    answer: "Road Runner",
-  },
-  {
-    question: "Which character loves honey and lives in the Hundred Acre Wood?",
-    options: ["Piglet", "Tigger", "Eeyore", "Winnie the Pooh"],
-    answer: "Winnie the Pooh",
-  },
-  {
-    question: "Who is Jerry's nemesis in the cartoon 'Tom and Jerry'?",
-    options: ["Tom", "Spike", "Butch", "Tyke"],
-    answer: "Tom",
-  },
-  {
-    question: "What type of animal is Baloo in 'The Jungle Book'?",
-    options: ["Bear", "Tiger", "Panther", "Wolf"],
-    answer: "Bear",
-  },
-  {
-    question: "Which superhero leads the Teen Titans?",
-    options: ["Cyborg", "Beast Boy", "Robin", "Starfire"],
-    answer: "Robin",
-  },
-  {
-    question: "In 'Finding Nemo', what type of fish is Nemo?",
-    options: ["Clownfish", "Goldfish", "Beta Fish", "Guppy"],
-    answer: "Clownfish",
-  },
-  {
-    question: "What is the name of the city where 'The Simpsons' live?",
-    options: ["Springfield", "Shelbyville", "Quahog", "Cohog"],
-    answer: "Springfield",
-  },
-];
-
-// Medium Questions
-let mediumQuestions = [
-  {
-    question: "In 'Adventure Time', who is Finn's best friend?",
-    options: ["Jake", "Princess Bubblegum", "Ice King", "Marceline"],
-    answer: "Jake",
-  },
-  {
-    question: "What is the secret identity of 'The Iron Giant'?",
-    options: [
-      "A War Machine",
-      "A Space Explorer",
-      "A Child's Imagination",
-      "A Protector",
-    ],
-    answer: "A Protector",
-  },
-  {
-    question:
-      "In 'Avatar: The Last Airbender', who is the only character able to bend all four elements?",
-    options: ["Zuko", "Toph", "Aang", "Katara"],
-    answer: "Aang",
-  },
-  {
-    question:
-      "Which character from 'Rick and Morty' is known for his catchphrase 'Wubba Lubba Dub Dub'?",
-    options: ["Morty", "Beth", "Jerry", "Rick"],
-    answer: "Rick",
-  },
-  {
-    question:
-      "In 'Gravity Falls', what is the name of the mysterious book Dipper finds?",
-    options: [
-      "Journal 3",
-      "The Mystery Guide",
-      "The Grimoire",
-      "Secrets of the Unknown",
-    ],
-    answer: "Journal 3",
-  },
-  {
-    question: "What is Gargamel's main goal in 'The Smurfs'?",
-    options: [
-      "To capture the Smurfs",
-      "To destroy the Smurf village",
-      "To become friends with the Smurfs",
-      "To steal Smurfette",
-    ],
-    answer: "To capture the Smurfs",
-  },
-  {
-    question: "Which character from 'Futurama' is a bending unit?",
-    options: ["Fry", "Leela", "Bender", "Zoidberg"],
-    answer: "Bender",
-  },
-  {
-    question:
-      "In 'Steven Universe', what are the gems on a mission to protect Earth from?",
-    options: [
-      "Alien Invaders",
-      "Environmental Collapse",
-      "Other Gems",
-      "Human Greed",
-    ],
-    answer: "Other Gems",
-  },
-  {
-    question: "In 'Regular Show', what are Mordecai and Rigby's occupations?",
-    options: [
-      "Park Managers",
-      "Groundskeepers",
-      "Convenience Store Clerks",
-      "Security Guards",
-    ],
-    answer: "Groundskeepers",
-  },
-  {
-    question: "Who is the main antagonist in 'Kim Possible'?",
-    options: ["Shego", "Dr. Drakken", "Monkey Fist", "Duff Killigan"],
-    answer: "Dr. Drakken",
-  },
-];
-
-// Hard Questions
-let hardQuestions = [
-  {
-    question: "In 'Samurai Jack', what is the name of Jack's main adversary?",
-    options: ["Aku", "The Shogun", "Demongo", "The Scotsman"],
-    answer: "Aku",
-  },
-  {
-    question:
-      "What year did the original 'Transformers' animated series first air?",
-    options: ["1982", "1984", "1986", "1988"],
-    answer: "1984",
-  },
-  {
-    question:
-      "In 'The Powerpuff Girls', what substance accidentally added gives the girls their powers?",
-    options: ["Sugar", "Spice", "Everything Nice", "Chemical X"],
-    answer: "Chemical X",
-  },
-  {
-    question:
-      "In 'Courage the Cowardly Dog', what is the name of Courage's owner?",
-    options: ["Muriel", "Eustace", "Shirley", "Katz"],
-    answer: "Muriel",
-  },
-  {
-    question: "Which 'SpongeBob SquarePants' character is a Texas native?",
-    options: [
-      "Sandy Cheeks",
-      "Patrick Star",
-      "Mr. Krabs",
-      "Squidward Tentacles",
-    ],
-    answer: "Sandy Cheeks",
-  },
-  {
-    question:
-      "In 'Ed, Edd n Eddy', for what purpose do the characters constantly try to scam their peers?",
-    options: [
-      "To buy candy",
-      "To build a clubhouse",
-      "To go on a vacation",
-      "To buy a car",
-    ],
-    answer: "To buy candy",
-  },
-  {
-    question:
-      "What is the true identity of the 'XJ-9' robot in 'My Life as a Teenage Robot'?",
-    options: [
-      "Jenny Wakeman",
-      "Nora Wakeman",
-      "Brad Carbunkle",
-      "Tuck Carbunkle",
-    ],
-    answer: "Jenny Wakeman",
-  },
-  {
-    question: "In 'Danny Phantom', what event turns Danny into a half-ghost?",
-    options: [
-      "A ghost portal accident",
-      "A chemical spill",
-      "A supernatural curse",
-      "An alien encounter",
-    ],
-    answer: "A ghost portal accident",
-  },
-  {
-    question:
-      "Which of these characters is not a member of 'The Fairly OddParents'?",
-    options: ["Timmy Turner", "Cosmo", "Wanda", "Jimmy Neutron"],
-    answer: "Jimmy Neutron",
-  },
-  {
-    question: "In 'Adventure Time', what is the name of the vampire queen?",
-    options: [
-      "Princess Bubblegum",
-      "Marceline",
-      "Flame Princess",
-      "Lumpy Space Princess",
-    ],
-    answer: "Marceline",
-  },
-];
-
-let selectedDifficulty = "easy";
-
-// Constants
-const SCORE_POINTS = 10;
-const MAX_QUESTIONS = 10;
-
-// Function to start the game
-function startGame(difficulty = "easy") {
-  questionCounter = 0;
-  score = 0;
-  availableQuestions = [...questions[difficulty]];
-  nextButton.classList.add("hide"); // Ensure the Next button is hidden at the start
-  nextQuestion();
-}
-
-// Function to get the next question
-function nextQuestion() {
-  acceptingAnswers = true;
-  if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-    localStorage.setItem("mostRecentScore", score);
-    return window.location.assign("/end-game.html"); // Adjust as necessary
-  }
-  // Update the question counter
-  questionCounter++;
-  questionCounterEl.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
-
-  // Get a random question from the available questions
-  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-  currentQuestion = availableQuestions[questionIndex];
-  questionEl.innerText = currentQuestion.question;
-
-  // Update the options with the current question's options
-  optionsEls.forEach((option, index) => {
-    option.innerText = currentQuestion.options[index];
-    option.parentElement.classList.remove("correct", "incorrect"); // Reset option visuals
-  });
-
-  // Remove the question from the available questions
-  availableQuestions.splice(questionIndex, 1);
-  nextButton.classList.add("hide"); // Hide Next button again for the new question
-}
-
-// Function to increment the score
-function selectOption(e) {
-    if (!acceptingAnswers) return;
+    {
+      question: "What is the name of Mickey Mouse's pet dog?",
+      options: ["Goofy", "Pluto", "Max", "Fido"],
+      answer: 2,
+    },
+    {
+      question: "Which cartoon character lives in a pineapple under the sea?",
+      options: [
+        "SpongeBob SquarePants",
+        "Patrick Star",
+        "Squidward Tentacles",
+        "Mr. Krabs",
+      ],
+      answer: 1,
+    },
+    {
+      question: "What color are the Smurfs?",
+      options: ["Green", "Blue", "Red", "Yellow"],
+      answer: 2,
+    },
+    {
+      question: "Who is the fastest character in Looney Tunes?",
+      options: ["Foghorn Leghorn", "Daffy Duck", "Road Runner", "Bugs Bunny"],
+      answer: 3,
+    },
+    {
+      question: "Which character loves honey and lives in the Hundred Acre Wood?",
+      options: ["Piglet", "Tigger", "Eeyore", "Winnie the Pooh"],
+      answer: 4,
+    },
+    {
+      question: "Who is Jerry's nemesis in the cartoon 'Tom and Jerry'?",
+      options: ["Tom", "Spike", "Butch", "Tyke"],
+      answer: 1,
+    },
+    {
+      question: "What type of animal is Baloo in 'The Jungle Book'?",
+      options: ["Bear", "Tiger", "Panther", "Wolf"],
+      answer: 1,
+    },
+    {
+      question: "Which superhero leads the Teen Titans?",
+      options: ["Cyborg", "Beast Boy", "Robin", "Starfire"],
+      answer: 3,
+    },
+    {
+      question: "In 'Finding Nemo', what type of fish is Nemo?",
+      options: ["Clownfish", "Goldfish", "Beta Fish", "Guppy"],
+      answer: 1,
+    },
+    {
+      question: "What is the name of the city where 'The Simpsons' live?",
+      options: ["Springfield", "Shelbyville", "Quahog", "Cohog"],
+      answer: 1,
+    },
+  ];
   
-    // Prevent multiple selections
-    acceptingAnswers = true;
-    const selectedOption = e.target;
-    const correctAnswer = currentQuestion.answer;
+  // Medium Questions
+  let mediumQuestions = [
+    {
+      question: "In 'Adventure Time', who is Finn's best friend?",
+      options: ["Jake", "Princess Bubblegum", "Ice King", "Marceline"],
+      answer: 1,
+    },
+    {
+      question: "What is the secret identity of 'The Iron Giant'?",
+      options: [
+        "A War Machine",
+        "A Space Explorer",
+        "A Child's Imagination",
+        "A Protector",
+      ],
+      answer: 4,
+    },
+    {
+      question:
+        "In 'Avatar: The Last Airbender', who is the only character able to bend all four elements?",
+      options: ["Zuko", "Toph", "Aang", "Katara"],
+      answer: 3,
+    },
+    {
+      question:
+        "Which character from 'Rick and Morty' is known for his catchphrase 'Wubba Lubba Dub Dub'?",
+      options: ["Morty", "Beth", "Jerry", "Rick"],
+      answer: 4,
+    },
+    {
+      question:
+        "In 'Gravity Falls', what is the name of the mysterious book Dipper finds?",
+      options: [
+        "Journal 3",
+        "The Mystery Guide",
+        "The Grimoire",
+        "Secrets of the Unknown",
+      ],
+      answer: 1,
+    },
+    {
+      question: "What is Gargamel's main goal in 'The Smurfs'?",
+      options: [
+        "To capture the Smurfs",
+        "To destroy the Smurf village",
+        "To become friends with the Smurfs",
+        "To steal Smurfette",
+      ],
+      answer: 1,
+    },
+    {
+      question: "Which character from 'Futurama' is a bending unit?",
+      options: ["Fry", "Leela", "Bender", "Zoidberg"],
+      answer: 3,
+    },
+    {
+      question:
+        "In 'Steven Universe', what are the gems on a mission to protect Earth from?",
+      options: [
+        "Alien Invaders",
+        "Environmental Collapse",
+        "Other Gems",
+        "Human Greed",
+      ],
+      answer: 3,
+    },
+    {
+      question: "In 'Regular Show', what are Mordecai and Rigby's occupations?",
+      options: [
+        "Park Managers",
+        "Groundskeepers",
+        "Convenience Store Clerks",
+        "Security Guards",
+      ],
+      answer: 2,
+    },
+    {
+      question: "Who is the main antagonist in 'Kim Possible'?",
+      options: ["Shego", "Dr. Drakken", "Monkey Fist", "Duff Killigan"],
+      answer: 2,
+    },
+  ];
   
-    // Check if the selected option is correct
-    const isCorrect = selectedOption.dataset['number'] == correctAnswer;
-  
-    // Increment the score if the answer is correct
-    if (isCorrect) {
-      incrementScore(SCORE_POINTS);
-      selectedOption.parentElement.classList.add('correct');
-    } else {
-      selectedOption.parentElement.classList.add('incorrect');
-    }
-  
-    // Show the Next button
-    optionsEls.forEach(option => {
-      if (option.dataset['number'] == correctAnswer) {
-        option.parentElement.classList.add('correct');
-      }
-    });
+  // Hard Questions
+  let hardQuestions = [
+    {
+      question: "In 'Samurai Jack', what is the name of Jack's main adversary?",
+      options: ["Aku", "The Shogun", "Demongo", "The Scotsman"],
+      answer: 1,
+    },
+    {
+      question:
+        "What year did the original 'Transformers' animated series first air?",
+      options: ["1982", "1984", "1986", "1988"],
+      answer: 2,
+    },
+    {
+      question:
+        "In 'The Powerpuff Girls', what substance accidentally added gives the girls their powers?",
+      options: ["Sugar", "Spice", "Everything Nice", "Chemical X"],
+      answer: 4,
+    },
+    {
+      question:
+        "In 'Courage the Cowardly Dog', what is the name of Courage's owner?",
+      options: ["Muriel", "Eustace", "Shirley", "Katz"],
+      answer: 1,
+    },
+    {
+      question: "Which 'SpongeBob SquarePants' character is a Texas native?",
+      options: [
+        "Sandy Cheeks",
+        "Patrick Star",
+        "Mr. Krabs",
+        "Squidward Tentacles",
+      ],
+      answer: 1,
+    },
+    {
+      question:
+        "In 'Ed, Edd n Eddy', for what purpose do the characters constantly try to scam their peers?",
+      options: [
+        "To buy candy",
+        "To build a clubhouse",
+        "To go on a vacation",
+        "To buy a car",
+      ],
+      answer: 1,
+    },
+    {
+      question:
+        "What is the true identity of the 'XJ-9' robot in 'My Life as a Teenage Robot'?",
+      options: [
+        "Jenny Wakeman",
+        "Nora Wakeman",
+        "Brad Carbunkle",
+        "Tuck Carbunkle",
+      ],
+      answer: 1,
+    },
+    {
+      question: "In 'Danny Phantom', what event turns Danny into a half-ghost?",
+      options: [
+        "A ghost portal accident",
+        "A chemical spill",
+        "A supernatural curse",
+        "An alien encounter",
+      ],
+      answer: 1,
+    },
+    {
+      question:
+        "Which of these characters is not a member of 'The Fairly OddParents'?",
+      options: ["Timmy Turner", "Cosmo", "Wanda", "Jimmy Neutron"],
+      answer: 4,
+    },
+    {
+      question: "In 'Adventure Time', what is the name of the vampire queen?",
+      options: [
+        "Princess Bubblegum",
+        "Marceline",
+        "Flame Princess",
+        "Lumpy Space Princess",
+      ],
+      answer: 2,
+    },
+  ];
